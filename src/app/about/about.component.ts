2be23824e3e77e17da71605495d89e9e631647cd
Sai Bhavana C <saibhavanac@Sais-MacBook-Air.local>
2024-07-14 12:15:25 +0530

    added files

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+//  declare const Typewriter : any;
+const Typewriter = require('../../../node_modules/t-writer.js/dist/t-writer.js');
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const writer = new Typewriter(target, {
+      loop: true, 
+      typeSpeed: 100 ,
+      typeColor : " #007bff "
+    })
+    
+    writer
+      .strings(
+        400,
+        "Web Developer",
+        "Software Developer", 
+        "ML Enthusiast"
+      )
+      .start()
+
+  }
+
+}
